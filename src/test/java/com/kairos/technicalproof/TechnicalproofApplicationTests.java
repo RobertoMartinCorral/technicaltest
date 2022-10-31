8e12962ac75ff3720cd11ece89abe28721c64ffe
Roberto Martín-Corral <roberto.martin-corral.mayoral.ext@ing.com>
2022-10-31 10:01:59 +0100

    feat(skeleton): Building block dependencies
    Added dependencies for:
      - spring
      - springboot
      - springboot starter web
      - springdoc
      - spring starter jpa
      - flyway
      - lombok
      - springboot starter tests

---
+++ src/test/java/com/kairos/technicalproof/TechnicalproofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos.technicalproof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalproofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

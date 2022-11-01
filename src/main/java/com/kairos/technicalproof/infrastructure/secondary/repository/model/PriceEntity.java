fbc6dbb7e9de2643cb47173d696ecc0baafea7dc
Roberto Martín-Corral <roberto.martin-corral.mayoral.ext@ing.com>
2022-11-01 11:55:54 +0100

    fix(infra): add primary key to entity
    Added a primary key for ddbb entity Prices.
    Removed old composite primary key

--- src/main/java/com/kairos/technicalproof/infrastructure/secondary/repository/model/PriceEntity.java
+++ src/main/java/com/kairos/technicalproof/infrastructure/secondary/repository/model/PriceEntity.java
@@ -6,28 +6,27 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import java.time.Instant;
+import java.util.UUID;
 
 @Entity(name="PRICES")
 @RequiredArgsConstructor
 @Getter
-@IdClass(PriceEntityId.class)
 public class PriceEntity {
 
     @Id
+    @Column(nullable=false, name="ID")
+    private final UUID id;
+
     @Column(nullable = false, name = "BRAND_ID")
     private final Long brandId;
 
-    @Id
     @Column(nullable = false, name = "PRICE_LIST")
     private final Long priceListId;
 
-    @Id
     @Column(nullable = false, name = "PRODUCT_ID")
     private final Long productId;
 
-    @Id
     @Column(nullable = false, name = "PRIORITY")
     private final Long priority;
 

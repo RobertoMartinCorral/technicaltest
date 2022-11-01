6f91577492b9daa14973cff15977f741502fb4e2
Roberto Martín-Corral <roberto.martin-corral.mayoral.ext@ing.com>
2022-11-01 11:19:02 +0100

    test(infra): Added test for secondary mapper
    Added the mapper for price secondary adapter, that converts between Price domain entity and its equivalent BBDD entity

---
+++ src/test/java/com/kairos/technicalproof/infrastructure/secondary/reposiroty/mapper/PriceMapperTest.java
@@ -0,0 +1,41 @@
+package com.kairos.technicalproof.infrastructure.secondary.reposiroty.mapper;
+
+import com.kairos.technicalproof.domain.core.Price;
+import com.kairos.technicalproof.infrastructure.secondary.repository.mapper.PriceMapper;
+import com.kairos.technicalproof.infrastructure.secondary.repository.model.PriceEntity;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PriceMapperTest {
+
+    @Test
+    public void mustMapToDomain() {
+
+        PriceEntity entity = new PriceEntity(
+                1L,
+                2L,
+                3L,
+                4L,
+                12.23,
+                "EUR",
+                Instant.now(),
+                Instant.now().plus(5, ChronoUnit.DAYS)
+        );
+
+        Price mappedPrice = new PriceMapper().mapToDomain(entity);
+
+        assertEquals(mappedPrice.getBrand().getId(), entity.getBrandId());
+        assertEquals(mappedPrice.getProduct().getId(), entity.getProductId());
+        assertEquals(mappedPrice.getPriceList().getId(), entity.getPriceListId());
+        assertEquals(mappedPrice.getAmount().getValue(), entity.getPrice());
+        assertEquals(mappedPrice.getAmount().getCurrency(), entity.getCurrency());
+        assertEquals(mappedPrice.getSchedule().getInitialDate(), entity.getStartDate());
+        assertEquals(mappedPrice.getSchedule().getFinalDate(), entity.getEndDate());
+
+    }
+
+}
